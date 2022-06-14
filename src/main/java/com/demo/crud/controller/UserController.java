e8028c5756dfac94426395f4b93d8e9a32c7565f
De Guia, Lili Anne <lili.anne.de.guia@accenture.com>
2022-06-14 08:40:13 +0800

    Added CORS

--- src/main/java/com/demo/crud/controller/UserController.java
+++ src/main/java/com/demo/crud/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @Slf4j
 @RestController
 @RequestMapping("/user")
